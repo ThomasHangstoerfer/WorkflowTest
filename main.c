a177de7c211bae6ec4b924ecbaddee92539b08e8
ThomasHangstoerfer <thomas@bitcoder.de>
2015-09-17 09:07:31 +0200

    Add main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void crash()
+{
+  volatile int* a = (int*)(NULL);
+  *a = 1;
+}
+
+int doSomething()
+{
+	printf("Now crashing...\n");
+	crash();
+	//printf("Now sleeping...\n");
+	//sleep(4000);
+	//printf("done.\n\n");
+}
+
+int test(int in)
+{
+	int res = 5 * in;
+	printf("test(%i): %i\n", in, res);
+	doSomething();
+	return res;
+}
+
+
+int main()
+{
+	printf("Hello World!\n");
+	test(3);
+	return 0;
+}
